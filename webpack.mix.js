a7fe97cce41f545f9c46c05cb61559625ec74efa
DominikIT <Dominik.Dziewulski@wp.pl>
2021-03-20 14:40:25 +0100

    Fixy

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,14 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-// mix.js('resources/js/app.js', 'public/js')
-//    .postCss('resources/css/app.css', 'public/css', [
-        //
-//    ]);
-
 // Kompilacja SCSS
 mix.sass('resources/scss/style.scss', 'public/assets/css');
 mix.sass('resources/scss/admin-login.scss', 'public/assets/css');
 mix.sass('resources/scss/admin-panel.scss', 'public/assets/css');
 
-mix.js('resources/js/app.js', 'public/assets/js');
